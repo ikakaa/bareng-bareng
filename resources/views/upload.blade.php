ea8886f365c30c0c37846800ef687ef3b673db5c
rishkaanggraeni <rishka.liono@binus.ac.id>
2022-03-22 16:10:59 +0700

    Add view IC & GB, update upload page
    Update upload page sesuai figma

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -12,10 +12,6 @@
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
             integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
         <link rel="stylesheet" href="style.css">
-        <script src="https://cdn.jsdelivr.net/npm/swiffy-slider@1.5.0/dist/js/swiffy-slider.min.js" crossorigin="anonymous"
-                defer></script>
-        <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.5.0/dist/css/swiffy-slider.min.css" rel="stylesheet"
-            crossorigin="anonymous">
 
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
@@ -31,35 +27,56 @@
 
         <div class="form-card  shadow justify-start flex flex-nowrap px-4 py-3 rounded w-2/3 " style="background:#F4F9E9">
         <form action="#" method="POST" enctype="multipart/form-data">
-            <div class="date-wrapper flex justify-between">
-                <div class="">
-            <label for="password" class="block sm:mb-2 mb-1 w-full  ">Start Date</label>
-            <input type="date" name="startdate" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth2  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="Your Password"></div>
-            <div class="ml-5">
-            <label for="password" class="block sm:mb-2 mb-1 w-full  ">End Date</label>
-            <input type="date" name="enddate" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth2  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="Your Password"></div>
-        </div>
-            <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">End Time</label>
-            <input type="time" name="endtime" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="Your Password">
-            <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">Product Name</label>
+            <label for="productname" class="block sm:mb-2 mb-1 w-full  mt-2">Product Name</label>
             <input type="text" name="productname" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="GMK-234 ">
-            <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">Short Description</label>
-            <input type="password" name="shortdesc" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="GMK-234 Up Close And Personal With Malaysia's Custom Keyboard Makers">
-            <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">Max Order</label>
-            <input type="number" name="maxorder" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="0">
-            <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">Minimum Order Quantity</label>
-            <input type="number" name="moq" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="0">
-            <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">Product Price(Inc. prox Indonesia Shipping Fee )</label>
+
+            <label for="productprice" class="block sm:mb-2 mb-1 w-full  mt-2">Product Price(Inc. prox Indonesia Shipping Fee )</label>
             <input type="number" name="productprice" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="10000">
 
-            <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">Product Price(Inc. prox Indonesia Shipping Fee )</label>
-           <select name="producttype" class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10">
+            <label for="shortdesc" class="block sm:mb-2 mb-1 w-full  mt-2">Short Description</label>
+            <input type="text" name="shortdesc" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="GMK-234 Up Close And Personal With Malaysia's Custom Keyboard Makers">
+
+            <label for="producttype" class="block sm:mb-2 mb-1 w-full  mt-2">Category</label>
+            <select name="producttype" class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10">
                <option value="customkeyboard">Custom Keyboard</option>
-               <option value="customdeskmat">Custom Desk Mat</option>
+               <option value="customdeskmat">Custom Deskmat</option>
                <option value="others">Others</option>
            </select>
-           <label for="password" class="block sm:mb-2 mb-1 w-full  mt-2">Product Price(Inc. prox Indonesia Shipping Fee )</label>
 
+           <label for="moq" class="block sm:mb-2 mb-1 w-full  mt-2">Minimum Order Quantity</label>
+           <input type="number" name="moq" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="0">
+          
+            <label for="maxorder" class="block sm:mb-2 mb-1 w-full  mt-2">Max Order (for stock)</label>
+            <input type="number" name="maxorder" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="0">
+            
+            <br><h3 style="font-size: 22px">Interest Check</h3>
+           
+            <label for="startdate" class="block sm:mb-2 mb-1 w-full  ">Start Date</label>
+            <input type="date" name="startdate" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth2  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10">
+        
+            <label for="enddate" class="block sm:mb-2 mb-1 w-full  ">End Date</label>
+            <input type="date" name="enddate" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth2  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10">
+
+            <label for="endtime" class="block sm:mb-2 mb-1 w-full  mt-2">End Time</label>
+            <input type="time" name="endtime" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10">
+            
+            <label for="shippingdate" class="block sm:mb-2 mb-1 w-full  ">Shipping Date</label>
+            <input type="date" name="shippingdate" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth2  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="Your Password">
+            
+        
+            <label for="discord" class="block sm:mb-2 mb-1 w-full  mt-2">Link Discord (for updates or chat)</label>
+            <input type="text" name="discord" required class="shadow  appearance-none border border-red rounded  py-2 sm:py-3 px-3 text-grey-darker mb-1 custwidth  block focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" placeholder="www.discord.com">
+            
+           <label for="uploadphoto" class="block sm:mb-2 mb-1 w-full  mt-2">Upload Photo</label>
+           <input type="file" name="photo" id="photo">
+
+    
+           <div class="button-right-bottom">
+                <button class="button-style">Upload</button>
+           </div>
+        
+
+         
 
         </form>
     </div>
